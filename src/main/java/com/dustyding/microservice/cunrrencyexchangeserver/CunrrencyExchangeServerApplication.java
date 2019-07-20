becfab12097797b06d92943a71b26b09ba572cb7
Ding-x <xiao_peng0202@hotmail.com>
2019-07-20 16:05:35 -0400

    init

---
+++ src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/CunrrencyExchangeServerApplication.java
@@ -0,0 +1,13 @@
+package com.dustyding.microservice.cunrrencyexchangeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunrrencyExchangeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CunrrencyExchangeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dustyding/microservice/cunrrencyexchangeserver/CunrrencyExchangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustyding.microservice.cunrrencyexchangeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunrrencyExchangeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
