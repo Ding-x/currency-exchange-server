b3f361ab9d47ae6d1663654b42b8a6720d36b60f
Ding-x <xiao_peng0202@hotmail.com>
2019-07-20 16:13:41 -0400

    currency built

---
+++ src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/ExchangeValue.java
@@ -0,0 +1,35 @@
+package com.dustyding.microservice.cunrrencyexchangeserver;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+    private String from, to;
+    private Long id;
+    private BigDecimal conversionMultiple;
+
+    public ExchangeValue() {
+    }
+
+    public ExchangeValue(String from, String to, Long id, BigDecimal conversionMultiple) {
+        this.from = from;
+        this.to = to;
+        this.id = id;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+}
