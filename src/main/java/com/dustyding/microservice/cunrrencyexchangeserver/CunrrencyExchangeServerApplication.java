63eee8c4a9def6f99a310ef4c9b1113a70bea900
Ding-x <xiao_peng0202@hotmail.com>
2019-07-21 15:45:42 -0400

    sleuth

--- src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/CunrrencyExchangeServerApplication.java
+++ src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/CunrrencyExchangeServerApplication.java
@@ -1,13 +1,20 @@
 package com.dustyding.microservice.cunrrencyexchangeserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CunrrencyExchangeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CunrrencyExchangeServerApplication.class, args);
 	}
-
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
--- src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/CurrencyExchangeController.java
+++ src/main/java/com/dustyding/microservice/cunrrencyexchangeserver/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.dustyding.microservice.cunrrencyexchangeserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private Environment environment;
 
@@ -21,6 +23,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to ){
         ExchangeValue value = repository.findByFromAndTo(from,to);
         value.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        logger.info("{}",value);
         return value;
     }
 }
